714aa8dc3e9b877905a96d31af75008864096ded
Joshua Snyder <joshua@posthog.com>
2025-06-10 12:57:14 +0200

    fix: put flushAt at 1

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,7 +4,7 @@ let _client: PostHog | undefined;
 
 export const getPostHogClient = () => {
     if (!_client) {
-        _client = new PostHog("sTMFPsFhdP1Ssg", { host: 'https://us.i.posthog.com' });
+        _client = new PostHog("sTMFPsFhdP1Ssg", { host: 'https://us.i.posthog.com', flushAt: 1, flushInterval: 0 });
     }
 
     return _client;
